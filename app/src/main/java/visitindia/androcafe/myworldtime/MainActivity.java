51814ea713347d2bd0977e533e0fb3b6f9d12e6c
Yayati09 <yaaytivs@gmail.com>
2018-10-06 12:31:38 +0530

    First Commit

--- app/src/main/java/visitindia/androcafe/myworldtime/MainActivity.java
+++ app/src/main/java/visitindia/androcafe/myworldtime/MainActivity.java
@@ -1,13 +1,36 @@
 package visitindia.androcafe.myworldtime;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+
+    TimeDiffAdapter timeDiffAdapter;
+
+    ArrayList<String> arrayList=new ArrayList<>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //  getActionBar().setDisplayShowHomeEnabled(true);
+
+        listView=findViewById(R.id.listview_time_diff);
+
+        String[] cityname=getResources().getStringArray(R.array.time_diff_array);
+
+        for(int i=0;i<cityname.length;i++)
+        {
+            arrayList.add(cityname[i]);
+        }
+
+        timeDiffAdapter=new TimeDiffAdapter(this,arrayList);
+
+        listView.setAdapter(timeDiffAdapter);
     }
 }
---
+++ app/src/main/java/visitindia/androcafe/myworldtime/TimeDiffAdapter.java
@@ -0,0 +1,151 @@
+package visitindia.androcafe.myworldtime;
+
+import android.content.Context;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class TimeDiffAdapter extends BaseAdapter{
+    Context context;
+    ArrayList<String> arrayList;
+
+
+    TextView time;
+    public TimeDiffAdapter(FragmentActivity activity, ArrayList<String> arrayList) {
+        context=activity;
+        this.arrayList=arrayList;
+    }
+
+    @Override
+    public int getCount() {
+        return arrayList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return arrayList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if(view==null)
+        {
+            LayoutInflater layoutInflater=LayoutInflater.from(context);
+            view=layoutInflater.inflate(R.layout.time_diff_list_row,null);
+        }
+
+        TextView cityname=view.findViewById(R.id.textview_city);
+        time=view.findViewById(R.id.textview_time);
+
+        cityname.setText(arrayList.get(i));
+        setTime(arrayList.get(i));
+
+        //time.setText(""+dateFormat);
+        return view;
+    }
+
+    private DateFormat setTime(String s) {
+        TimeZone timeZone = null;
+
+        if(s.equalsIgnoreCase("Mexico City"))
+        {
+            timeZone=TimeZone.getTimeZone("America/Mexico_City");
+        }
+        if(s.equalsIgnoreCase("tokyo")) {
+            timeZone = TimeZone.getTimeZone("Asia/Tokyo");
+        }
+        if(s.equalsIgnoreCase("new york")) {
+            timeZone = TimeZone.getTimeZone("America/New_York");
+        }
+        if(s.equalsIgnoreCase("kabul")) {
+            timeZone = TimeZone.getTimeZone("Asia/Kabul");
+        }
+        if(s.equalsIgnoreCase("seoul")) {
+            timeZone = TimeZone.getTimeZone("Asia/Seoul");
+        }
+        if(s.equalsIgnoreCase("kathmandu"))
+        {
+            timeZone=TimeZone.getTimeZone("Asia/Kathmandu");
+        }
+        if(s.equalsIgnoreCase("islamabad"))
+        {
+            timeZone=TimeZone.getTimeZone("Asia/Karachi");
+        }
+        if(s.equalsIgnoreCase("Bangkok"))
+        {
+            timeZone=TimeZone.getTimeZone("Asia/Bangkok");
+        }
+        if(s.equalsIgnoreCase("beijing"))
+        {
+            timeZone=TimeZone.getTimeZone("Asia/Shanghai");
+        }
+        if(s.equalsIgnoreCase("Cairo"))
+        {
+            timeZone=TimeZone.getTimeZone("Africa/Cairo");
+        }
+        if(s.equalsIgnoreCase("paris"))
+        {
+            timeZone=TimeZone.getTimeZone("Europe/Paris");
+        }
+        if(s.equalsIgnoreCase("london"))
+        {
+            timeZone=TimeZone.getTimeZone("Europe/London");
+        }
+        if(s.equalsIgnoreCase("kuala_Lumpur"))
+        {
+            timeZone=TimeZone.getTimeZone("Asia/Kuala_Lumpur");
+        }
+        if(s.equalsIgnoreCase("dhaka"))
+        {
+            timeZone=TimeZone.getTimeZone("Asia/Dhaka");
+        }
+        if(s.equalsIgnoreCase("colombo"))
+        {
+            timeZone=TimeZone.getTimeZone("Asia/Colombo");
+        }
+        if(s.equalsIgnoreCase("dubai"))
+        {
+            timeZone=TimeZone.getTimeZone("Asia/Dubai");
+        }
+        if(s.equalsIgnoreCase("berlin"))
+        {
+            timeZone=TimeZone.getTimeZone("Europe/Berlin");
+        }
+        if(s.equalsIgnoreCase("moscow"))
+        {
+            timeZone=TimeZone.getTimeZone("Europe/Moscow");
+        }
+        if(s.equalsIgnoreCase("baghdad"))
+        {
+            timeZone=TimeZone.getTimeZone("Asia/Baghdad");
+        }
+
+
+        final Date now = new Date();
+        final DateFormat format = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
+
+        if(timeZone!=null) {
+            format.setTimeZone(timeZone);
+            String str=""+format.format(now);
+            System.out.println(""+format.format(now));
+            time.setText(""+str.substring(str.length()-8,str.length()));
+        }
+
+        return  format;
+    }
+
+
+}
